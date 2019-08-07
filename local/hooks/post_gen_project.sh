fcc9b9c30782832835bcbcdc43b666690825f814
Maarten van der Sande <maartenvandersande@hotmail.com>
2019-08-07 10:53:33 +0200

    cookiecutter templates, closes #2

---
+++ local/hooks/post_gen_project.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# if no snakemake configuration
+if [ ! -d "$HOME/.config/snakemake/" ]; then
+  mkdir "$HOME/.config/snakemake/"
+fi
+
+# if the profile already exists delete it
+if [ -d "$HOME/.config/snakemake/{{cookiecutter.profile_name}}" ]; then
+  rm -r "$HOME/.config/snakemake/{{cookiecutter.profile_name}}/"
+fi
+
+# make a directory for the profile
+mkdir "$HOME/.config/snakemake/{{cookiecutter.profile_name}}"
+
+# now place it in the snakemake profiles
+cd ..
+mv -r {{cookiecutter.profile_name}} "$HOME/.config/snakemake/{{cookiecutter.profile_name}}"
